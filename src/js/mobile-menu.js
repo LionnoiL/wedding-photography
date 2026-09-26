3cb1a87389d76c84dee392331826ee16d56ecdb1
Nazar Zaverukha <violin2003@ukr.net>
2026-09-27 01:14:56 +0300

    Fix header styles per code review: BEM naming, logo size, font-weight, gap

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,9 +1,7 @@
-const burgerBtn = document.querySelector('.burger-btn');
+const burgerBtn = document.querySelector('.header__burger-btn');
 const mobileMenu = document.querySelector('.mobile-menu');
-const closeBtn = document.querySelector('.mobile-menu-close');
-const menuLinks = document.querySelectorAll(
-  '.mobile-menu-link, .mobile-menu-btn'
-);
+const closeBtn = document.querySelector('.mobile-menu__close-btn');
+const menuLinks = document.querySelectorAll('.mobile-menu__link, .mobile-menu__button');
 
 function openMenu() {
   mobileMenu.classList.add('is-open');
